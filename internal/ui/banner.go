783c7d021b6eb9c1ced116dd8b9e437a620be980
Woeter <skippedlead@gmail.com>
2026-03-15 01:20:36 +0530

    add ascii banner logic

---
+++ internal/ui/banner.go
@@ -0,0 +1,33 @@
+package ui
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+const Version = "0.1.0"
+
+var banner = `
+  ___       ___
+ |_ _|_ __ |_ _|
+  | || '_ \ | |
+  | || | | || |
+ |___|_| |_|___|`
+
+func PrintBanner() {
+	gradient := lipgloss.NewStyle().
+		Foreground(Primary).
+		Bold(true)
+
+	version := lipgloss.NewStyle().
+		Foreground(Secondary).
+		Italic(true).
+		Render(fmt.Sprintf("  v%s", Version))
+
+	tagline := MutedStyle.Render("  Blazing fast project initializer")
+
+	fmt.Println(gradient.Render(banner) + version)
+	fmt.Println(tagline)
+	fmt.Println()
+}
