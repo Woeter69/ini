b3d8b68af15c7e1257158162708932a4be74a2d9
Woeter <skippedlead@gmail.com>
2026-03-15 01:21:17 +0530

    add csharp dotnet initialization logic

---
+++ internal/handler/csharp.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/Woeter69/ini/internal/scaffold"
+	"github.com/Woeter69/ini/internal/ui"
+)
+
+func init() { Register("csharp", &CSharpHandler{}) }
+
+type CSharpHandler struct{}
+
+func (c *CSharpHandler) Name() string    { return "C#" }
+func (c *CSharpHandler) Validate() error {
+	if _, err := exec.LookPath("dotnet"); err != nil {
+		return fmt.Errorf("dotnet is not installed or not in PATH.\n  Install it: https://dotnet.microsoft.com/download")
+	}
+	return nil
+}
+
+func (c *CSharpHandler) Init(config ProjectConfig) error {
+	fmt.Printf("  %s Creating C# project with dotnet...\n", ui.Arrow)
+	dotnetNew := exec.Command("dotnet", "new", "console", "-n", config.Name, "-o", config.Path, "--force")
+	dotnetNew.Stdout = nil
+	dotnetNew.Stderr = os.Stderr
+	if err := dotnetNew.Run(); err != nil {
+		return fmt.Errorf("failed to create dotnet project: %w", err)
+	}
+	fmt.Printf("  %s C# project created\n", ui.CheckMark)
+
+	if err := scaffold.WriteGitignore(config.Path, config.Language); err != nil {
+		return err
+	}
+	if err := scaffold.WriteReadme(config.Path, config.Name, config.Language); err != nil {
+		return err
+	}
+	if config.Git {
+		if err := scaffold.InitGit(config.Path); err != nil { return err }
+	}
+
+	fmt.Println()
+	s := strings.Builder{}
+	s.WriteString(ui.SuccessStyle.Render("🚀 Your C# project is ready!"))
+	s.WriteString("\n\n")
+	s.WriteString(fmt.Sprintf("  cd %s\n", config.Name))
+	s.WriteString("  dotnet run\n")
+	fmt.Println(ui.SummaryBox.Render(s.String()))
+	fmt.Println()
+	return nil
+}
